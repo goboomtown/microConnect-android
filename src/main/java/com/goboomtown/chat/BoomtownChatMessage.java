631cd528d19c0065fe85ed27bc901f7b42dc8c91
tenacious <fbeachler@gmail.com>
2017-06-14 19:57:28 -0400

    catch null object reference causing NPE in BoomtownChatMessage

--- src/main/java/com/goboomtown/chat/BoomtownChatMessage.java
+++ src/main/java/com/goboomtown/chat/BoomtownChatMessage.java
@@ -2,10 +2,7 @@ package com.goboomtown.chat;
 
 import android.util.Log;
 
-import com.goboomtown.chat.JSONHelper;
-
 import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.packet.PacketExtension;
 import org.jivesoftware.smackx.delay.packet.DelayInformation;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,29 +21,27 @@ import java.util.TimeZone;
  * Created by larry on 2015-12-16.
  */
 public class BoomtownChatMessage {
-    public String   id;
-    public String   type;
-    public String	from;
-    public String   fromName;
-    public String   fromMention;
-    public String	text;
-    public Date     date;
-    public String   dateString;
-    public String   xml;
-    public String   avatar;
-    public String   preview;
-    public String   url;
-    public boolean  self;
+    public String id;
+    public String type;
+    public String from;
+    public String fromName;
+    public String fromMention;
+    public String text;
+    public Date date;
+    public String dateString;
+    public String xml;
+    public String avatar;
+    public String preview;
+    public String url;
+    public boolean self;
     public DelayInformation delayInformation;
 
-    public BoomtownChatMessage()
-    {
+    public BoomtownChatMessage() {
 
     }
 
-    public void populateFromMessage(Message message)
-    {
-        if ( message.hasStanzaIdSet() )
+    public void populateFromMessage(Message message) {
+        if (message.hasStanzaIdSet())
             this.id = message.getStanzaId();
         else {
 //            this.id = java.util.UUID.randomUUID().toString();
@@ -54,7 +49,7 @@ public class BoomtownChatMessage {
 
         String body = message.getBody();
         String from = message.getFrom();
-        if ( from == null )
+        if (from == null)
             from = "";
         String[] tokens = from.split("/");
         String resource;
@@ -64,31 +59,21 @@ public class BoomtownChatMessage {
             resource = tokens[0];
         tokens = resource.split(";");
         this.from = tokens[0];
-//        this.fromName = getParticipantInfo(this.from, "name");
-//        this.fromMention = getParticipantInfo(this.from, "alias");
-//        if ( this.fromName == null )
-//            this.fromName = this.from;
-//        if ( this.fromMention == null )
-//            this.fromMention = this.from;
-
         this.type = message.getType().toString();
         this.text = body;
         this.xml = message.toString();
-
-        //  <delay xmlns='urn:xmpp:delay' stamp='2016-04-14T18:02:05.443+00:00' from='comm.5unzbf@conference.xmpp.dev.gizmocreative.com/users:RMG;jko4v2'></delay>
+        //  <delay xmlns='urn:xmpp:delay' stamp='2016-04-14T18:02:05.443+00:00' from='comm.5unzbf@conference.xmpp.dev.goboomtown.com/users:RMG;jko4v2'></delay>
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-        if ( message.hasExtension("delay", "urn:xmpp:delay") ) {
+        if (message.hasExtension("delay", "urn:xmpp:delay")) {
             delayInformation = (DelayInformation) message.getExtension("delay", "urn:xmpp:delay");
             this.date = delayInformation.getStamp();
-        }
-        else
+        } else
             this.date = new Date();
         this.dateString = sdf.format(this.date);
     }
 
-    public void populateFromString(String message)
-    {
+    public void populateFromString(String message) {
         Map<String, Object> entry;
         try {
             JSONObject jsonObject = new JSONObject(message);
@@ -104,81 +89,66 @@ public class BoomtownChatMessage {
             this.url = (String) entry.get("url");
             this.dateString = (String) entry.get("dateString");
 
-            if ( this.dateString == null ) {
+            if (this.dateString == null) {
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
                 sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-//            if ( message.hasExtension("delay", "urn:xmpp:delay") ) {
-//                message.getExtension("delay", "urn:xmpp:delay");
-//            }
-//            else
                 this.date = new Date();
                 this.dateString = sdf.format(this.date);
             }
-
-//            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
-//            sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-//            if ( message.hasExtension("delay", "urn:xmpp:delay") ) {
-//                message.getExtension("delay", "urn:xmpp:delay");
-//            }
-//            else
-//                this.date = new Date();
-//            this.dateString = sdf.format(this.date);
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    private static final String kPayloadTypePlain           = "plain";
-    private static final String kPayloadTypeSms             = "sms";
-    private static final String kPayloadTypeNotification    = "notification";
-    private static final String kPayloadTypeAttachment      = "attachment";
+    private static final String kPayloadTypePlain = "plain";
+    private static final String kPayloadTypeSms = "sms";
+    private static final String kPayloadTypeNotification = "notification";
+    private static final String kPayloadTypeAttachment = "attachment";
 
     public Boolean postprocessMessage() {
-        String  secretPlain         = md5(kPayloadTypePlain);
-        String  secretSms 			= md5(kPayloadTypeSms);
-        String  secretNotification 	= md5(kPayloadTypeNotification);
-        String  secretAttachment    = md5(kPayloadTypeAttachment);
-        Boolean rc                  = false;
+        String secretPlain = md5(kPayloadTypePlain);
+        String secretSms = md5(kPayloadTypeSms);
+        String secretNotification = md5(kPayloadTypeNotification);
+        String secretAttachment = md5(kPayloadTypeAttachment);
+        Boolean rc = false;
 
         JSONObject object = null;
         try {
             object = new JSONObject(text);
-            if( object instanceof JSONObject ) {
+            if (object instanceof JSONObject) {
                 String secret = object.optString("secret");
-                if ( secret!=null && (secret.equals(secretPlain) || secret.equals(secretNotification) || secret.equals(secretAttachment)) ) {
+                if (secret != null && (secret.equals(secretPlain) || secret.equals(secretNotification) || secret.equals(secretAttachment))) {
                     this.text = "";
                     String text = object.optString("message");
-                    if ( text!=null && !text.equals("null") )
+                    if (text != null && !text.equals("null"))
                         this.text = text;
                     else
                         this.text = "";
                     JSONObject context = object.optJSONObject("context");
-                    if ( context != null )
-                    {
+                    if (context != null) {
                         String from = context.optString("from");
-                        if ( from!=null && !from.equalsIgnoreCase("null"))
+                        if (from != null && !from.equalsIgnoreCase("null"))
                             this.from = from;
 
                         String fromName = context.optString("from_name");
-                        if ( fromName != null )
+                        if (fromName != null)
                             this.fromName = fromName;
 
                         String avatar = context.optString("from_avatar");
-                        if ( avatar != null )
+                        if (avatar != null)
                             this.avatar = avatar;
                     }
                     JSONObject attachment = object.optJSONObject("attachment");
-                    if ( attachment != null )
-                    {
+                    if (attachment != null) {
                         String preview = attachment.optString("preview");
-                        if ( preview!=null && !preview.equalsIgnoreCase("null"))
+                        if (preview != null && !preview.equalsIgnoreCase("null"))
                             this.preview = preview;
 
                         String url = attachment.optString("url");
-                        if ( url!=null  && !url.equalsIgnoreCase("null") )
+                        if (url != null && !url.equalsIgnoreCase("null"))
                             this.url = url;
                     }
-                    Log.d("postprocessMessage", this.from+":"+this.text);
+                    Log.d("postprocessMessage", this.from + ":" + this.text);
                     rc = true;
                     return rc;
 
@@ -230,51 +200,46 @@ public class BoomtownChatMessage {
         return "";
     }
 
-    public String toString()
-    {
+    public String toString() {
         JSONObject jsonObject = new JSONObject();
         try {
-            jsonObject.put("id",            this.id);
-            jsonObject.put("type",          this.type);
-            jsonObject.put("from",          this.from);
-            jsonObject.put("fromName",      this.fromName);
-            jsonObject.put("fromMention",   this.fromMention);
-            jsonObject.put("text",          this.text);
-            jsonObject.put("dateString",    this.dateString);
-            jsonObject.put("xml",           this.xml);
-            jsonObject.put("preview",       this.preview);
-            jsonObject.put("url",           this.url);
+            jsonObject.put("id", this.id);
+            jsonObject.put("type", this.type);
+            jsonObject.put("from", this.from);
+            jsonObject.put("fromName", this.fromName);
+            jsonObject.put("fromMention", this.fromMention);
+            jsonObject.put("text", this.text);
+            jsonObject.put("dateString", this.dateString);
+            jsonObject.put("xml", this.xml);
+            jsonObject.put("preview", this.preview);
+            jsonObject.put("url", this.url);
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return jsonObject.toString();
     }
 
-    public String getDateString(Date date)
-    {
+    public String getDateString(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
         return sdf.format(date);
     }
 
     public void trimText() {
-        if ( text != null )
+        if (text != null)
             text = text.trim();
     }
 
-    public String getMessage()
-    {
+    public String getMessage() {
         return text;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return fromName;
     }
 
-    public String getDate()
-    {
-        if ( dateString != null )
+    public String getDate() {
+        if (dateString != null)
             return dateString;
         else {
             date = new Date();
@@ -287,7 +252,7 @@ public class BoomtownChatMessage {
         Date date = null;
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-        if ( dateString != null ) {
+        if (dateString != null) {
             try {
                 date = sdf.parse(dateString);
                 sdf.applyPattern("LLL d, yyyy KK:mm a");
@@ -298,14 +263,13 @@ public class BoomtownChatMessage {
             date = new Date();
             return DateFormat.getDateTimeInstance().format(date);
         }
-        if ( date == null )
+        if (date == null)
             date = new Date();
         sdf.applyPattern("LLL d, yyyy KK:mm a");
         return sdf.format(date);
     }
 
-    public boolean isMe()
-    {
+    public boolean isMe() {
         return self;
     }
 }
